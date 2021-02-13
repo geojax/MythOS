da2315f82afcbf5d3306c08eb9da67afa8269a04
Alexander Su <sualexander2013@gmail.com>
2021-02-13 01:01:32 -0800

    Midnight Pushes
    sleep is for the week

--- TextBasedAdventure/main.cpp
+++ TextBasedAdventure/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 #include "PlotNode.h"
 #include "Rooms.h"
+
+string readFile(string fileName){ //prints out dscrp and gets exit choices 
+
+	string myText = "";
+	ifstream MyReadFile("Map.txt");
+
+
+	while (myText[0] != '[') {
+		cout << myText << "\n";
+		getline(MyReadFile, myText);
+	}
+
+	return myText;
+	MyReadFile.close();
+}
+
+
+
 int main() {
+
+	string exits = readFile("Map.txt");
+	int n = 0;
+
+	for (int i = 1; exits[i] != ']'; ++i) {
+		if (exits[i] == ',')
+		{
+			++n;
+		}
+		exits[n] += exits[i];
+	} 
+
+	int input = 0;
+
+	cin >> input;
+
+	ifstream MyReadFile2(exits[input - 1] + ".txt");
+	getline(MyReadFile2, myText);
+	cout << myText;
+
 	return 0;
 }
