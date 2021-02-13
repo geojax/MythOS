e76dd140fc45e33af0c5169506ee42e0e397e8bb
geojax <50026101+geojax@users.noreply.github.com>
2021-02-13 10:53:13 -0800

    Fixed infinity catching

---
+++ TextBasedAdventure/Header.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MAX_LINES_OF_DESCRIPTION 200
+string readFile(string filename) { // prints out description and returns exit choices 
+
+	string myText = "";
+	ifstream RoomFile(filename);
+	int i = 0;
+	while (myText[0] != '[') {
+		cout << myText << '\n';
+		getline(RoomFile, myText);
+		if (++i > MAX_LINES_OF_DESCRIPTION) {
+			cout << "ERROR: Description for " + filename + ".txt is too long or missing end statement!";
+			return "";
+		}
+	}
+
+	return myText; // myText holds the exits as a string
+	RoomFile.close();
+}
+
+/*Cycle through exits string, look for one less comma than user's input*/
+			/* add each character to selectedExit
+			if we reach a comma, selectedExit becomes an empty string
+			Subtract 1 from input at each comma
+			if input == 0, return "[selectedExit].txt"*/
+string getExit(string exits, int input) { /* get exit from string of exits */
+	string selectedExit = "";
+	for (int i = 1; exits[i] != ']'; ++i) {
+		if (exits[i] == ',')
+		{
+			--input;
+			if (input > 0)
+				selectedExit = "";
+		}
+		if (input == 0)
+		{
+			return selectedExit;
+		}
+		selectedExit += exits[i];
+	}
+}
--- TextBasedAdventure/main.cpp
+++ TextBasedAdventure/main.cpp
@@ -1,48 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include "PlotNode.h"
-#include "Rooms.h"
+
+#include "Header.h"
 
 using namespace std;
-#define MAX_LINES_OF_DESCRIPTION 200
-string readFile(string filename){ // prints out description and returns exit choices 
 
-	string myText = "";
-	ifstream RoomFile(filename);
-	int i = 0;
-	while (myText[0] != '[') {
-		cout << myText;
-		getline(RoomFile, myText);
-		if (++i > 200) {
-			cout << "ERROR: Description for " + filename + ".txt is too long or missing end statement!";
-			return "";
-		}
-	}
-
-	return myText; // myText holds the exits as a string
-	RoomFile.close();
-}
-
-/*Cycle through exits string, look for one less comma than user's input*/
-			/* add each character to selectedExit
-			if we reach a comma, selectedExit becomes an empty string
-			Subtract 1 from input at each comma
-			if input == 0, return "[selectedExit].txt"*/
-string getExit(string exits, int input) { /* get exit from string of exits */
-	string selectedExit = "";
-	for (int i = 1; exits[i] != ']'; ++i) {
-		if (exits[i] == ',')
-		{
-			--input;
-			selectedExit = "";
-		}
-		if (input == 0)
-		{
-			return selectedExit;
-		}
-		selectedExit += exits[i];
-	}
-}
 
 int main() {
 
