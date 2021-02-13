945dff2717badea634f4691a7accf3fa4120e269
Alexander Su <sualexander2013@gmail.com>
2021-02-13 11:51:54 -0800

    potato

--- TextBasedAdventure/main.cpp
+++ TextBasedAdventure/main.cpp
@@ -10,10 +10,30 @@ int main() {
 
 	string exits = readFile("Map.txt"); // exits as one long string, commas in between
 	// we need a throw/catch statement here
-	int input = 0;
-	cin >> input;
 
-	readFile(getExit(exits, input) + ".txt");
+	bool gameRunning;
+
+	if (exits == "Map.txt");
+	{
+		gameRunning = true;
+	}
+
+	while (gameRunning == true)
+	{
+		string sInput = "";
+		cin >> sInput;
+
+		int Input;
+		getInput(sInput, Input);
+
+		exits = readFile(getExit(exits, Input) + ".txt");
+
+		if (exits == "End")
+		{
+			gameRunning == false;
+		}
+
+	}
 
 	return 0;
 }
